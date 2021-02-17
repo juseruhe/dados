b0532aa00b7c81facbf176549fdb66dce105bbda
Sebastian <jsruiz7411@misena.edu.co>
2021-02-17 14:54:40 -0500

    Dados con funciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoIzquierda = '../assets/dados/dado1.png';
+  dadoDerecha = '../assets/dados/dado4.png';
+  numero1:number;
+  numero2:number;
+
+  tirarDados(): void{
+
+    this.numero1 = Math.round( Math.random() *5)+1;
+    this.numero2 =Math.round(Math.random() * 5)+1;
+
+    console.log(this.numero1, this.numero2);
+  }
+
 }
