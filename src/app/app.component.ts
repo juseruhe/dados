c85391908d018f28c02f0bbb1a76c936a4193df8
Sebastian <jsruiz7411@misena.edu.co>
2021-02-17 15:20:24 -0500

    Dado terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   dadoIzquierda = '../assets/dados/dado1.png';
   dadoDerecha = '../assets/dados/dado4.png';
-  numero1:number;
-  numero2:number;
+  numero1 = 1;
+  numero2 = 2;
 
   tirarDados(): void{
 
     this.numero1 = Math.round( Math.random() *5)+1;
     this.numero2 =Math.round(Math.random() * 5)+1;
 
-    console.log(this.numero1, this.numero2);
+    this.dadoIzquierda = '../assets/dados/dado'+this.numero1+'.png';
+    this.dadoDerecha= '../assets/dados/dado'+this.numero2+'.png';
   }
 
 }
